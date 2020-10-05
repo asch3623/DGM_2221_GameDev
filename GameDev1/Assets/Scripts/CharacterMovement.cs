f1391078368b2d5fa8a9840598e6078860d38e65
Annika <10848892@my.uvu.edu>
2020-10-05 17:14:08 -0600

    Walk and Run

---
+++ GameDev1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(TrailRenderer))]
+public class CharacterMovement : MonoBehaviour
+{
+  private Rigidbody rb;
+  public float moveSpeed = 10f;
+  private int seconds = 1, coolDownSeconds = 7;
+  public BoolData coolDown, isDarting;
+  private TrailRenderer trail;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    trail = GetComponent<TrailRenderer>();
+   trail.emitting = false;
+    coolDown.value = false;
+  }
+
+  private void Update()
+  
+  {
+    rb.transform.rotation = Quaternion.Euler(0,0,0);
+    
+    float moveH = Input.GetAxis("Horizontal");
+    float moveV = Input.GetAxis("Vertical");
+    Vector3 movement = new Vector3(moveH, 0f, moveV)* Time.deltaTime * moveSpeed;
+    rb.MovePosition(transform.position + movement);
+    
+    
+    
+    
+    if (Input.GetKeyDown(KeyCode.LeftShift) && !isDarting.value && !coolDown.value)
+           {
+             isDarting.value = true;
+             trail.emitting = true;
+             moveSpeed = 50f;
+             StartCoroutine(Dart());
+           }
+
+  }
+
+  IEnumerator Dart()
+  {
+    yield return new WaitForSeconds(seconds);
+    moveSpeed = 10f;
+    isDarting.value = false;
+    trail.emitting = false;
+    coolDown.value = true;
+    yield return new WaitForSeconds(coolDownSeconds);
+    coolDown.value = false;
+  }
+}
---
+++ GameDev1/Assets/Scripts/FillImageBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FillImageBehavior : MonoBehaviour
+{
+  private Image im;
+  public BoolData coolDown, isDart;
+  private float fillValue = 1f;
+  private float maxFill = 1.1f, minFill = -0.1f;
+
+  private float seconds = .5f;
+ 
+    void Start()
+    {
+
+        im = GetComponent<Image>();
+        im.fillAmount = fillValue;
+        print(fillValue);
+        isDart.value = false;
+    }
+
+    void Update()
+    {
+        if (isDart.value)
+        {
+            StartCoroutine(decreaseFillVal());
+      
+            im.fillAmount = fillValue;
+            return;
+        }
+        if (coolDown.value && !isDart.value)
+        {
+            
+           StartCoroutine(increaseFillVal());
+           im.fillAmount = fillValue;
+        }
+    }
+
+    IEnumerator decreaseFillVal()
+    {
+        while (isDart.value && fillValue >= minFill)
+                    {
+                        fillValue -= 0.1f;
+                        im.fillAmount = fillValue;
+                        yield return new WaitForSeconds(seconds);
+                    }
+    }
+    IEnumerator increaseFillVal()
+    {
+        while (coolDown.value && fillValue <= maxFill)
+        {
+            yield return new WaitForSeconds(1);
+            fillValue += 0.001f;
+            im.fillAmount = fillValue;
+            
+        }
+    }
+}
