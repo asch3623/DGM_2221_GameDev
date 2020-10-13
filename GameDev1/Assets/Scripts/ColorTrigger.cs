b354e0f5eb9f7f38f5b763fe4c0266b39d138992
Annika <10848892@my.uvu.edu>
2020-10-12 19:27:08 -0600

    TriggerEvents

---
+++ GameDev1/Assets/Scripts/ColorTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ColorTrigger : MonoBehaviour
+{
+
+private MeshRenderer mat;
+private Shader shad;
+ 
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mat.material.color = Color.green;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        mat.material.color = Color.white;
+        mat.m
+    }
+}
