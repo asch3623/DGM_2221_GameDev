fbc420eb3ee8efec4d61f473d9e985d75d665f7c
Annika <10848892@my.uvu.edu>
2020-11-20 22:44:10 -0700

    update

--- GameDev1/Assets/Scripts/GameActionHandler.cs
+++ GameDev1/Assets/Scripts/GameActionHandler.cs
@@ -4,13 +4,13 @@ using UnityEngine.Events;
 // code from DGM 2670;
 public class GameActionHandler : MonoBehaviour
 {
-    public GameAction[] gameAction;
-    public UnityEvent[] handlerEvent;
+    public GameAction gameAction;
+    public UnityEvent handlerEvent;
     public float holdTime;
 
     private void Start()
     {
-        gameAction[a].action += ActionHandler;
+        gameAction.action += ActionHandler;
     }
 
     private void ActionHandler()
