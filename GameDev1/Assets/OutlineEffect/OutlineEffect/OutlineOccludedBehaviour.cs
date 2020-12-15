bc1ddd008960d9fd6c0d68e2cd8d4b8f5b7fac24
Annika <10848892@my.uvu.edu>
2020-12-14 23:16:45 -0700

    Camera outline

---
+++ GameDev1/Assets/OutlineEffect/OutlineEffect/OutlineOccludedBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using cakeslice;
+using UnityEngine;
+
+public class OutlineOccludedBehaviour : MonoBehaviour
+{
+    public Outline playerOutline;
+
+    private void Start()
+    {
+        playerOutline.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, 7f, LayerMask.GetMask("CamWall")))
+        {
+            playerOutline.enabled = true;
+        }
+        else
+        {
+            playerOutline.enabled = false;
+        }
+    }
+}
---
+++ GameDev1/Assets/Scripts/BadgeObj.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class BadgeObj : ScriptableObject
+{
+    public string description;
+    public bool isObtained;
+}
